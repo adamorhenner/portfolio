3d67d5c5407e61c64973b0d7da48e4c3dbdca045
adamorhenner <adamorhenner2@outlook.com>
2024-10-21 08:21:40 -0300

    react typing effect

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import NavbarComponent from "@/components/NavBar";
 import ImageWithIcons from "@/components/ImageWithIcons";
 import {FaDatabase, FaNodeJs, FaReact} from "react-icons/fa";
+import ReactTypingEffect from 'react-typing-effect';
 
 const sectionStyle = {
     height: '100vh',
@@ -42,6 +43,8 @@ const TitleContainer = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
+    width: 50%;
+
 `;
 
 const Home = () => {
@@ -56,13 +59,21 @@ const Home = () => {
                             <p>Hello, I am</p>
                             <p>&lt; Adamor </p>
                             <p>Henner /&gt;</p>
-                            <p>&gt; Fullstack Developer</p>
+                            <ReactTypingEffect
+                                text={['> Fullstack Developer']}
+                                speed={100}
+                                eraseDelay={10}
+                                typingDelay={500}
+                                displayTextRenderer={(text, i) => {
+                                    return <p>{text}</p>;
+                                }}
+                            />
                         </TitleDiv>
                     </TitleContainer>
                     <ImageWithIcons
-                        icon1={<FaReact size={40} color="#61DBFB" />}
-                        icon2={<FaNodeJs size={30} color="#68A063" />}
-                        icon3={<FaDatabase size={60} color="#4DB33D" />}
+                        icon1={<FaReact size={40} color="#61DBFB"/>}
+                        icon2={<FaNodeJs size={30} color="#68A063"/>}
+                        icon3={<FaDatabase size={60} color="#4DB33D"/>}
                     />
                 </div>
             </section>
@@ -85,7 +96,7 @@ const Home = () => {
                 <p>Formulário de contato ou links para LinkedIn e e-mail...</p>
             </section>
         </div>
-);
+    );
 };
 
 export default Home;
